2521f86db3ef600d0516d26b0fda08dfc349568d
ooha-m <omanduri@sysgain.com>
2017-01-19 15:30:28 +0530

    update

--- mariadb-latest/scripts/wp-script.sh
+++ mariadb-latest/scripts/wp-script.sh
@@ -2,13 +2,11 @@
 apt-get update
 apt-get install -y gcc make automake apache2 php5 php5-mysql php5-gd libssh2-php libapache2-mod-php5 php5-mcrypt
 service apache2 restart
-sleep 10
 apt-get install -y software-properties-common
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 add-apt-repository 'deb http://ftp.kaist.ac.kr/mariadb/repo/5.5/ubuntu precise main'
 apt-get update
 apt-get install -y mariadb-client
-sleep 20
 #------------------------------------------------------------------------------------------
 clear
 #echo "============================================"
@@ -36,8 +34,32 @@ curl -O https://wordpress.org/latest.tar.gz
 tar -zxvf latest.tar.gz
 #change dir to wordpress
 cd wordpress
-#copy file to parent dir
-cp -rf . ..
-#move back to parent dir
-cd ..
-#remove files from wordpress folder
+#create wp config
+cp wp-config-sample.php wp-config.php
+#set database details with perl find and replace
+##perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
+##perl -pi -e "s/username_here/$dbuser/g" wp-config.php
+##perl -pi -e "s/password_here/$dbpass/g" wp-config.php
+
+#set WP salts
+##perl -i -pe'
+##  BEGIN {
+##    @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
+##    push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
+##    sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
+## }
+##  s/put your unique phrase here/salt()/ge
+##' wp-config.php
+
+#create uploads folder and set permissions
+mkdir wp-content/uploads
+chmod 775 wp-content/uploads
+echo "Cleaning..."
+#remove zip file
+rm /var/www/latest.tar.gz
+#remove bash script
+#rm wp.sh
+echo "========================="
+echo "Installation is complete."
+echo "========================="
+#fi
