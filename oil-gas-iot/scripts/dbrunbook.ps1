32122bd67c81f49e2b48f42384d941e8b973bd96
ooha-m <omanduri@sysgain.com>
2018-06-07 16:38:54 +0530

    Update dbrunbook.ps1

--- oil-gas-iot/scripts/dbrunbook.ps1
+++ oil-gas-iot/scripts/dbrunbook.ps1
@@ -45,20 +45,20 @@
     $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $azurePassword)
     start-Sleep -s 20
     Login-AzureRmAccount -TenantId $tenantId -SubscriptionID $subscriptionId -Credential $psCred
-    start-Sleep -s 20
+    start-Sleep -s 40
         
     ## Creating Database
     $AzureSQLServerName = $sqlServerName + ".database.windows.net,1433"
     $ScriptURL="https://raw.githubusercontent.com/BlueMetal/iot-edge-dynocard/master/code/containers/mssql_db/create-dynocard-db-azure.sql"
     $ScriptFromGit = Invoke-WebRequest -Uri $ScriptURL -UseBasicParsing
-    start-Sleep -s 20
+    start-Sleep -s 40
     Invoke-Sqlcmd -ServerInstance $AzureSQLServerName -Database "master" -Username "sqladmin" -Password "Password@1234" -Query $ScriptFromGit.Content
     start-Sleep -s 40
 
     ## Creating Schema
     $ScriptURL2="https://raw.githubusercontent.com/BlueMetal/iot-edge-dynocard/master/code/containers/mssql_db/create-dynocard-schema.sql" 
     $ScriptFromGit2 = Invoke-WebRequest -Uri $ScriptURL2 -UseBasicParsing
-    start-Sleep -s 20
+    start-Sleep -s 40
     Invoke-Sqlcmd -ServerInstance $AzureSQLServerName -Database "db4cards" -Username "sqladmin" -Password "Password@1234" -Query $ScriptFromGit2.Content 
     }
 }
